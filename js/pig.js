9d53ce045c4d55c53351f50b71788b4755a9e12e
Kevine Musoni <kevinenicky@gmail.com>
2018-09-30 23:02:34 +0300

    easy and hard levels added

--- js/pig.js
+++ js/pig.js
@@ -1,24 +1,21 @@
 $(document).ready(function() {
-	console.log("Doc ready");
-	function rollDice(){
-		console.log("init");
-		var dice1 = document.getElementById("dice1");
-		var dice2 = document.getElementById("dice2");
-		var result = document.getElementById("result");
-		var d1 = Math.floor(Math.random() * 6) + 1;
-		var d2 = Math.floor( Math.random() * 6) + 1;
-		var dice_total = d1 + d2;
-
-		dice1.innerHTML = d1;
-		dice2.innerHTML = d2;
-		result.innerHTML = "You have rolled" + dice_total+ ".";
-
-		if(d1 == d2){
-			result.innerHTML += "Double! another turn!"
-	     }
-	 }
-
-	 $("#btn").click(function(){
-	 	rollDice();
-	 })
-});	
+console.log("Doc ready");
+function rollDice(){
+console.log("init");
+var dice1 = document.getElementById("dice1");
+var dice2 = document.getElementById("dice2");
+var result = document.getElementById("result");
+var d1 = Math.floor(Math.random() * 6) + 1;
+var d2 = Math.floor( Math.random() * 6) + 1;
+var dice_total = d1 + d2;
+dice1.innerHTML = d1;
+dice2.innerHTML = d2;
+result.innerHTML = "You have rolled" + dice_total+ ".";
+if(d1 == d2){
+result.innerHTML += "Double! another turn!"
+}
+}
+$("#btn").click(function(){
+rollDice();
+})
+});
