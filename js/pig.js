1145f57398124a9f3aeba68fb90f01c09fe52c48
Kevine Musoni <kevinenicky@gmail.com>
2018-10-01 10:21:03 +0300

    rolling dice1

--- js/pig.js
+++ js/pig.js
@@ -1,21 +1,38 @@
 $(document).ready(function() {
-console.log("Doc ready");
-function rollDice(){
-console.log("init");
+
+function rollDice1(){
 var dice1 = document.getElementById("dice1");
 var dice2 = document.getElementById("dice2");
-var result = document.getElementById("result");
+var result1 = document.getElementById("result1");
 var d1 = Math.floor(Math.random() * 6) + 1;
 var d2 = Math.floor( Math.random() * 6) + 1;
-var dice_total = d1 + d2;
+var dice1_total = d1;
 dice1.innerHTML = d1;
 dice2.innerHTML = d2;
-result.innerHTML = "You have rolled" + dice_total+ ".";
+result1.innerHTML = "You have rolled" + dice1_total+ ".";
 if(d1 == d2){
-result.innerHTML += "Double! another turn!"
+result1.innerHTML += "Double! another turn!"
 }
 }
-$("#btn").click(function(){
-rollDice();
+$("#btn1").click(function(){
+rollDice1();
+})
+
+function rollDice2(){
+var dice1 = document.getElementById("dice1");
+var dice2 = document.getElementById("dice2");
+var result2 = document.getElementById("result2");
+var d1 = Math.floor(Math.random() * 6) + 1;
+var d2 = Math.floor( Math.random() * 6) + 1;
+var dice2_total = d2;
+dice1.innerHTML = d1;
+dice2.innerHTML = d2;
+result2.innerHTML = "You have rolled" + dice_total2+ ".";
+if(d1 == d2){
+result2.innerHTML += "Double! another turn!"
+}
+}
+$("#btn2").click(function(){
+rollDice2();
 })
 });
